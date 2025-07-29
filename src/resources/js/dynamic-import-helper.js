4a0697086d42ff3757e12135a80917de449174e6
Raveesh Yadav <raveeshyadav8@outlook.com>
2025-07-29 21:55:54 +0530

    fixed dynamic import

--- src/resources/js/dynamic-import-helper.js
+++ src/resources/js/dynamic-import-helper.js
@@ -1,10 +1,27 @@
-const pages = import.meta.glob('/resources/js/Modules/**/*.vue');
+// Import all Vue components from both Pages and Modules
+const pages = import.meta.glob('/resources/js/Pages/**/*.vue');
+const modulePages = import.meta.glob('/resources/js/Modules/**/Pages/*.vue');
 
 export default async function resolvePageComponent(name) {
-    const path = `/resources/js/Modules/${name}.vue`;
-    if (!pages[path]) {
-        throw new Error(`Unknown dynamic import path for module: ${name}`);
+    // First try to find the component in regular Pages
+    const pagePath = `/resources/js/Pages/${name}.vue`;
+    if (pages[pagePath]) {
+        return await pages[pagePath]();
     }
 
-    return await pages[path]();
+    // If not found in Pages, try to find in Modules
+    const modulePath = `/resources/js/Modules/${name}.vue`;
+    if (modulePages[modulePath]) {
+        return await modulePages[modulePath]();
+    }
+
+    // If still not found, try to find in module Pages directories
+    for (const path in modulePages) {
+        const moduleName = path.split('/').pop().replace('.vue', '');
+        if (moduleName === name) {
+            return await modulePages[path]();
+        }
+    }
+
+    throw new Error(`Unknown dynamic import path for component: ${name}`);
 }
